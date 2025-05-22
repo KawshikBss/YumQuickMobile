fdfb4545e4ad6e9b0dc27a8bf342ebea2361a655
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-23 04:44:12 +0600

    renamed

--- lib/features/launch/presentation/screens/launch_screen.dart
+++ lib/features/launch/presentation/screens/launch_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yumquick/shared/theme/app_colors.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class LaunchScreen extends StatelessWidget {
+  const LaunchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
