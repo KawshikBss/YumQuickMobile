2a40bc46e8521cacb32c749f93a693f03052a934
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-23 04:44:00 +0600

    completed router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,16 @@
 import 'package:go_router/go_router.dart';
+import 'package:yumquick/core/routes/app_route.dart';
+import 'package:yumquick/core/routes/app_routes.dart';
 
-GoRouter router = GoRouter(routes: []);
+GoRouter appRouter = GoRouter(
+  routes:
+      AppRoutes.all
+          .map(
+            (AppRoute route) => GoRoute(
+              name: route.name,
+              path: route.path,
+              builder: (context, state) => route.page,
+            ),
+          )
+          .toList(),
+);
