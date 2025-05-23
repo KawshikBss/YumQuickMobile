3de9559f638cbb116c798a4660eacf3412d90086
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-24 04:27:41 +0600

    added new colors

--- lib/shared/theme/app_colors.dart
+++ lib/shared/theme/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color yellowBase = Color(0xfff5cb58);
   static const Color yellowSecondary = Color(0xfff3e9b5);
   static const Color orangeBase = Color(0xffe95322);
+  static const Color orangeSecondary = Color(0xffFFDECF);
 
   static const Color textLight = Color(0xfff8f8f8);
   static const Color textDark = Color(0xff391713);
