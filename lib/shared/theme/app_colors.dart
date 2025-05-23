15a1a19a872c9de52d0bba49b436d3b48106a86a
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-23 14:33:23 +0600

    added new color

--- lib/shared/theme/app_colors.dart
+++ lib/shared/theme/app_colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/widgets.dart';
 
 class AppColors {
   static const Color yellowBase = Color(0xfff5cb58);
+  static const Color yellowSecondary = Color(0xfff3e9b5);
   static const Color orangeBase = Color(0xffe95322);
 }
