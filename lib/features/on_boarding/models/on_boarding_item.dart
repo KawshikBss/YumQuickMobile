cea6108e382dd35662422ea8a44dd3f4d405a2a9
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-24 00:14:43 +0600

    class for onboarding items

---
+++ lib/features/on_boarding/models/on_boarding_item.dart
@@ -0,0 +1,13 @@
+class OnBoardingItem {
+  final String title;
+  final String content;
+  final String image;
+  final String icon;
+
+  const OnBoardingItem({
+    required this.title,
+    required this.content,
+    required this.image,
+    required this.icon,
+  });
+}
