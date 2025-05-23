4730329756c60240e1c02c47e393b4756e4a4d2d
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-24 04:28:01 +0600

    created login form

---
+++ lib/features/auth/presentation/widgets/login_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:yumquick/features/auth/presentation/widgets/auth_input.dart';
+import 'package:yumquick/features/auth/presentation/widgets/submit_button.dart';
+import 'package:yumquick/shared/constants/app_fonts.dart';
+import 'package:yumquick/shared/theme/app_colors.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AuthInput(
+          label: 'Email or Mobile Number',
+          hintText: 'example@example.com',
+        ),
+        const SizedBox(height: 22),
+        AuthInput(label: 'Password', hintText: 'password', secured: true),
+        const SizedBox(height: 14),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot Password',
+              style: TextStyle(
+                fontFamily: AppFonts.leagueSpartan,
+                color: AppColors.orangeBase,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 40),
+        SubmitButton(label: 'Log In'),
+      ],
+    );
+  }
+}
