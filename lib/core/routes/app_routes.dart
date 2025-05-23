a2681639438625b3748fcb90fafaba3f8c699295
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-24 04:30:17 +0600

    added signup route

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:yumquick/core/routes/app_route.dart';
 import 'package:yumquick/features/auth/presentation/pages/login_screen.dart';
+import 'package:yumquick/features/auth/presentation/pages/signup_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/launch_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/welcome_screen.dart';
 import 'package:yumquick/features/on_boarding/presentation/screens/on_boarding_screen.dart';
@@ -14,5 +15,6 @@ class AppRoutes {
     ),
     AppRoute(name: 'Welcome Screen', path: '/welcome', page: WelcomeScreen()),
     AppRoute(name: 'Log In', path: '/login', page: LoginScreen()),
+    AppRoute(name: 'Sign Up', path: '/signup', page: SignupScreen()),
   ];
 }
