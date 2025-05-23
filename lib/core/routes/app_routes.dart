c58e3bc47d0d29b863e50ef5138cc4837e614776
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-24 04:25:25 +0600

    added login route and modified other routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,20 +1,18 @@
 import 'package:yumquick/core/routes/app_route.dart';
+import 'package:yumquick/features/auth/presentation/pages/login_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/launch_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/welcome_screen.dart';
 import 'package:yumquick/features/on_boarding/presentation/screens/on_boarding_screen.dart';
 
 class AppRoutes {
   static final List<AppRoute> all = [
+    AppRoute(name: 'On Boarding Screen', path: '/', page: OnBoardingScreen()),
     AppRoute(
       name: 'First Screen',
       path: '/splash-screen',
       page: LaunchScreen(),
     ),
-    AppRoute(name: 'Welcome Screen', path: '/', page: WelcomeScreen()),
-    AppRoute(
-      name: 'On Boarding Screen',
-      path: '/on-boarding',
-      page: OnBoardingScreen(),
-    ),
+    AppRoute(name: 'Welcome Screen', path: '/welcome', page: WelcomeScreen()),
+    AppRoute(name: 'Log In', path: '/login', page: LoginScreen()),
   ];
 }
