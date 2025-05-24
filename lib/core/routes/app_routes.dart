6c3d868606436e827e11f8de9969491fc3aabb6a
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-25 00:03:22 +0600

    added set password route

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:yumquick/core/routes/app_route.dart';
 import 'package:yumquick/features/auth/presentation/pages/login_screen.dart';
+import 'package:yumquick/features/auth/presentation/pages/set_password_screen.dart';
 import 'package:yumquick/features/auth/presentation/pages/signup_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/launch_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/welcome_screen.dart';
@@ -16,5 +17,10 @@ class AppRoutes {
     AppRoute(name: 'Welcome Screen', path: '/welcome', page: WelcomeScreen()),
     AppRoute(name: 'Log In', path: '/login', page: LoginScreen()),
     AppRoute(name: 'Sign Up', path: '/signup', page: SignupScreen()),
+    AppRoute(
+      name: 'Set Password',
+      path: '/set-password',
+      page: SetPasswordScreen(),
+    ),
   ];
 }
