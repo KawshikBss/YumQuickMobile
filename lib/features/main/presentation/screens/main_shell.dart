58445311d1d69022cd915bc034ed77826ad9ae57
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-26 18:33:48 +0600

    add MainShell widget to manage child routes

---
+++ lib/features/main/presentation/screens/main_shell.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainShell extends StatelessWidget {
+  final Widget child;
+  const MainShell({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: child);
+  }
+}
