8a362cc85abd55c2e275d4c293e5dfa216428fc3
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-26 18:33:29 +0600

    added main shell route and child routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,11 +1,17 @@
 import 'package:yumquick/core/routes/app_route.dart';
+import 'package:yumquick/features/all_foods/presentation/screens/all_foods_screen.dart';
 import 'package:yumquick/features/auth/presentation/screens/login_screen.dart';
 import 'package:yumquick/features/auth/presentation/screens/set_fingerprint_screen.dart';
 import 'package:yumquick/features/auth/presentation/screens/set_password_screen.dart';
 import 'package:yumquick/features/auth/presentation/screens/signup_screen.dart';
+import 'package:yumquick/features/favorites/presentation/screens/favorites_screen.dart';
+import 'package:yumquick/features/help/presentation/screens/help_screen.dart';
+import 'package:yumquick/features/home/presentation/screens/home_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/launch_screen.dart';
 import 'package:yumquick/features/launch/presentation/screens/welcome_screen.dart';
+import 'package:yumquick/features/main/presentation/screens/main_shell.dart';
 import 'package:yumquick/features/on_boarding/presentation/screens/on_boarding_screen.dart';
+import 'package:yumquick/features/orders/presentation/screens/orders_screen.dart';
 
 class AppRoutes {
   static final List<AppRoute> all = [
@@ -28,5 +34,19 @@ class AppRoutes {
       path: '/set-fingerprint',
       page: SetFingerprintScreen(),
     ),
+    AppRoute(
+      name: 'Main',
+      path: '/main',
+      pageBuilder: (context, state, child) => MainShell(child: child),
+      shellChildren: mainShellRoutes,
+    ),
+  ];
+
+  static final List<AppRoute> mainShellRoutes = [
+    AppRoute(name: 'Home', path: '/home', page: HomeScreen()),
+    AppRoute(name: 'All Foods', path: '/foods/all', page: AllFoodsScreen()),
+    AppRoute(name: 'Favourites', path: '/favourites', page: FavoritesScreen()),
+    AppRoute(name: 'Orders', path: '/orders/all', page: OrdersScreen()),
+    AppRoute(name: 'Help', path: '/help', page: HelpScreen()),
   ];
 }
