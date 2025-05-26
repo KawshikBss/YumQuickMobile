05d287890b1c25a0a937702d8aefa2f3dd387b9f
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-26 18:33:08 +0600

    modified for shell routes

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,16 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:yumquick/core/routes/app_route.dart';
 import 'package:yumquick/core/routes/app_routes.dart';
 
 GoRouter appRouter = GoRouter(
+  initialLocation: '/welcome',
   routes:
-      AppRoutes.all
-          .map(
-            (AppRoute route) => GoRoute(
-              name: route.name,
-              path: route.path,
-              builder: (context, state) => route.page,
-            ),
-          )
-          .toList(),
+      AppRoutes.all.map((AppRoute route) {
+        if (route.shellChildren.isNotEmpty) {
+          return ShellRoute(
+            builder:
+                route.pageBuilder ??
+                (context, state, child) => route.page ?? const Placeholder(),
+            routes:
+                route.shellChildren
+                    .map(
+                      (child) => GoRoute(
+                        name: child.name,
+                        path: child.path,
+                        builder:
+                            (context, state) =>
+                                child.page ?? const Placeholder(),
+                      ),
+                    )
+                    .toList(),
+          );
+        } else {
+          return GoRoute(
+            name: route.name,
+            path: route.path,
+            builder: (context, state) => route.page ?? const Placeholder(),
+          );
+        }
+      }).toList(),
 );
