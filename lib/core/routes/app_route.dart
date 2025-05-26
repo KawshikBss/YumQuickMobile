4191057a3f15e544d7b075d96e6948182d4b8fad
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-26 18:32:54 +0600

    modified for shell routes

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 
 class AppRoute {
   final String name;
   final String path;
-  final Widget page;
+  final Widget? page;
+  final Widget Function(BuildContext, GoRouterState, Widget)? pageBuilder;
   final IconData? icon;
   final bool requiresAuth;
+  final List<AppRoute> shellChildren;
 
   const AppRoute({
     required this.name,
     required this.path,
-    required this.page,
+    this.page,
+    this.pageBuilder,
     this.icon,
     this.requiresAuth = false,
+    this.shellChildren = const [],
   });
 }
