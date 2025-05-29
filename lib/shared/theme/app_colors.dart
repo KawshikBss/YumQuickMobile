c2974e04d34b2455b7e80fe84c55901220d986ef
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-29 13:45:17 +0600

    added text grey

--- lib/shared/theme/app_colors.dart
+++ lib/shared/theme/app_colors.dart
@@ -8,6 +8,7 @@ class AppColors {
 
   static const Color textLight = Color(0xfff8f8f8);
   static const Color textDark = Color(0xff391713);
+  static const Color textGrey = Color(0xff676767);
 
   static const Color bgLight = Color(0xfff8f8f8);
   static const Color bgDark = Color(0xff391713);
