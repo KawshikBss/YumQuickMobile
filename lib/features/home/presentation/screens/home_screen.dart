08fa4efe1378949f50098769273bdac4bb70d3e1
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-30 03:07:08 +0600

    completed by adding all widgets

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:yumquick/features/home/presentation/widgets/home_content.dart';
+import 'package:yumquick/features/home/presentation/widgets/best_seller_section.dart';
+import 'package:yumquick/features/home/presentation/widgets/categories_section.dart';
 import 'package:yumquick/features/home/presentation/widgets/home_header.dart';
+import 'package:yumquick/features/home/presentation/widgets/offers_section.dart';
+import 'package:yumquick/features/home/presentation/widgets/recommended_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(slivers: [HomeHeader(), HomeContent()]);
+    return CustomScrollView(
+      slivers: [
+        const HomeHeader(),
+        const CategoriesSection(),
+        const BestSellerSection(),
+        const OffersSection(),
+        const RecommendedSection(),
+      ],
+    );
   }
 }
