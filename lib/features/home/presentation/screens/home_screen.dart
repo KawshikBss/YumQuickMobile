5c3c7301e61d647acce6ee802baa80ea757b2383
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-29 15:42:13 +0600

    added home content

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yumquick/features/home/widgets/home_content.dart';
+import 'package:yumquick/features/home/widgets/home_header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return const CustomScrollView(slivers: [HomeHeader(), HomeContent()]);
   }
 }
