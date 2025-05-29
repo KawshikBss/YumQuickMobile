b7641202efa0d6b9342b2e2ee4fd553f5a01eb21
Kawshik Biswas <kawshikbss@gmail.com>
2025-05-29 20:02:14 +0600

    modified

--- lib/features/home/models/offer_item.dart
+++ lib/features/home/models/offer_item.dart
@@ -22,12 +22,6 @@ class OfferItem {
       thumbnail: 'https://images.unsplash.com/photo-1513104890138-7c749659a591',
     ),
     OfferItem(
-      title: 'Taste the freshness today',
-      offerPrice: '20% Off',
-      thumbnail:
-          'https://images.unsplash.com/photo-1580476262798-bddd9f4b7369?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    ),
-    OfferItem(
       title: 'Healthy and tasty choice',
       offerPrice: 'Free Drink Included',
       thumbnail: 'https://images.unsplash.com/photo-1550547660-d9450f859349',
